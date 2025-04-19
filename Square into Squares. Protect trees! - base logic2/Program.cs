cdb11c8a138d5b8704caade4984f48d5ca63787c
dkostadinov <d78mdd@gmail.com>
2025-04-19 15:24:47 +0300

    "Square into Squares. Protect trees!" - base logic2 - gets lists of consecutive numbers whose squares' sum is equal to a number's square

--- Square into Squares. Protect trees! - base logic2/Program.cs
+++ Square into Squares. Protect trees! - base logic2/Program.cs
@@ -24,42 +24,13 @@
 
 // at least 2 numbers
 
-
-///////////////////////////////////////////////////////////
-
-// strictly increasing sequence
-
-// so that the sum of the numbers is equal to n-1
-
-// 2 + 3 + 4 + 5 +.....
-
-// would compare all these with n-1
-// 2 
-// 2 + 3
-// 2 + 3 + 4
-// 2 + 3 + 4 + 5
-// 2 + 3 + 5
-// 2 + 4 
-// 2 + 4 + 5 !
-// 2 + 5
-// 3
-// 3 + 4
-// 3 + 4 + 5
-// 3 + 5
-// 4
-// 4 + 5
-// 5
-
-// at least 1 number
-
-
 namespace Square_into_Squares._Protect_trees____base_logic2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int n = 31;
+            int n = 11;
 
             Console.WriteLine(n);
 
@@ -76,17 +47,18 @@ namespace Square_into_Squares._Protect_trees____base_logic2
                 Console.WriteLine();
             }
 
-            /* gets lists of consecutive numbers equal to a number
-             *11
-               1 2 3 4
-               1 2 7
-               1 3 6
-               1 4 5
-               1 9
+            /* gets lists of consecutive numbers whose squares' sum is equal to a number's square
+             * 11
+               1 2 4 6 8
+               1 2 4 10
              */
         }
 
+        // number to decompose
         private static int n;
+
+        // n squared
+        private static int ns;
 
         private static List<List<int>> results = new List<List<int>>();
 
@@ -94,6 +66,7 @@ namespace Square_into_Squares._Protect_trees____base_logic2
         public static List<int> decompose(int n)
         {
             Program.n = n;
+            Program.ns = n * n;
 
             AddToSum(new List<int>() { 1 });
 
@@ -104,21 +77,37 @@ namespace Square_into_Squares._Protect_trees____base_logic2
         {
             List<int> nums = new List<int>(numbers);
 
-            if (nums.Sum() == n)
+            //var sum = nums.Sum();
+            var sum = Sum(nums);
+            if (sum == ns)
             {
                 results.Add(nums);
             }
-            else if (nums.Sum() < n)
+            else if (sum < ns)
             {
                 int nextNum = nums[nums.Count - 1] + 1;
 
-                for (int i = nextNum; i + nums.Sum() <= n; i++)
+                for (int i = nextNum; i + sum <= ns; i++)
                 {
                     nums.Add(i);
                     AddToSum(nums);
                     nums.Remove(i);
                 }
             }
+        }
+
+
+        // return the sum of the squares of the numbers in the list
+        public static int Sum(List<int> nums)
+        {
+            int sum = 0;
+
+            foreach (int num in nums)
+            {
+                sum += num * num;
+            }
+
+            return sum;
         }
 
     }
