801564c5efd1cd41d61533037bb95705a7f8d607
dkostadinov <d78mdd@gmail.com>
2025-04-20 17:15:39 +0300

    initial overlapping implementation

--- Sum of Intervals/Program.cs
+++ Sum of Intervals/Program.cs
@@ -7,26 +7,70 @@ namespace Sum_of_Intervals
 
         static void Main(string[] args)
         {
-            Console.WriteLine(SumIntervals(new (int, int)[] { (4, 4), (6, 6), (8, 8) }));
+            Console.WriteLine(SumIntervals(new (int, int)[] { (1, 4), (7, 10), (3, 5) }));
         }
+
+        // implement overlapping  
+        // (should work also for containing intervals?)
+        // (are they sorted?)
+        /*
+         * add all intervals to a list
+         * get 1by1
+         *  compare with all others - by left's end and right's start
+         *  if >
+         *  then remove both and add new with left's start and right's end
+         */
 
         public static int SumIntervals((int, int)[] intervals)
         {
-            int result = -1;
+            List<(int, int)> asList = intervals.ToList();
 
-            int[] lengths = new int[intervals.Length];
+            List<(int, int)> combined = new List<(int, int)>();
 
-            for (int i = 0; i < intervals.Length; i++)
+            for (int i = 0; i < asList.Count; i++)
             {
-                (int, int) interval = intervals[i];
-                
-                lengths[i] = interval.Item2 - interval.Item1;
+                (int, int) intervalCurrent = asList[i];
+                bool overlap = false;
+                int begC = -1;
+                int endC = -1;
+                int begO = -1;
+                int endO = -1;
+                (int, int) intervalOther = new ValueTuple<int, int>(-1, -1);
+
+                begC = intervalCurrent.Item1;
+                endC = intervalCurrent.Item2;
+
+
+                for (int j = 0; j < asList.Count; j++)
+                {
+                    intervalOther = asList[j];
+
+                    begO = intervalOther.Item1;
+                    endO = intervalOther.Item2;
+
+                    overlap = endC > begO;
+
+                    if (overlap)
+                    {
+                        break;
+                    }
+                }
+
+
+                if (overlap)
+                {
+                    combined.Add((begC, endO)); // add a combined interval
+                }
+                else // no overlapping
+                {
+                    combined.Add((begC, endC)); // just add the current interval
+                }
             }
 
-            result = lengths.Sum();
-
-            return result;
+            return -1;
         }
+
+
 
     }
 }
