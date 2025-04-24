b3213a927e7a1bc53ae3b83461f83be2b1df0f6c
dkostadinov <d78mdd@gmail.com>
2025-04-24 23:01:11 +0300

    initial frame

--- The observed PIN/Program.cs
+++ The observed PIN/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace The_observed_PIN
+﻿// https://www.codewars.com/kata/5263c6999e0f40dee200059d/train/csharp
+
+namespace The_observed_PIN
 {
     internal class Program
     {
@@ -12,7 +14,33 @@
     {
         public static List<string> GetPINs(string observed)
         {
-            return null;
+            int observedInt = int.Parse(observed);
+
+            List<int> result = new List<int>();
+
+            /* 
+             * if 1 then 2 and 4
+             * if 2 then 1,3,5
+             * if 3 then 2,6
+             * if 4 then 1,5,7
+             * if 5 then 2,4,6,8
+             * if 6 then 3,5,9
+             * if 7 then 4,8
+             * if 8 then 5,7,9,0
+             * if 9 then 6,8
+             * if 0 then 8
+             */
+
+            for (int i = 0; i < UPPER; i++)
+            {
+                
+            }
+
+
+
+            return result
+                .Select(i => i.ToString())
+                .ToList();
         }
     }
 
