9bef99e393be560e39c7f124aa7e0df9792cd5f2
dkostadinov <d78mdd@gmail.com>
2025-04-26 19:17:07 +0300

    init Battleship field validator

---
+++ Battleship field validator - tests/UnitTest1.cs
@@ -0,0 +1,25 @@
+using Battleship_field_validator;
+
+namespace Battleship_field_validator___tests
+{
+    public class Tests
+    {
+        [Test]
+        public void TestCase()
+        {
+            int[,] field = new int[10, 10]
+            {{1, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+                {1, 0, 1, 0, 0, 0, 0, 0, 1, 0},
+                {1, 0, 1, 0, 1, 1, 1, 0, 1, 0},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+                {0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+                {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+            Assert.That(BattleshipField.ValidateBattlefield(field), Is.True);
+        }
+
+    }
+}
---
+++ Battleship field validator/Program.cs
@@ -0,0 +1,22 @@
+﻿// https://www.codewars.com/kata/52bb6539a4cf1b12d90005b7/train/csharp
+
+namespace Battleship_field_validator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class BattleshipField
+    {
+        public static bool ValidateBattlefield(int[,] field)
+        {
+            // Write your magic here
+            return true;
+        }
+
+    }
+}
