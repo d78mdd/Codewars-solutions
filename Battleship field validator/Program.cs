ccf4404e4806a26a1cb4e7554f7fff448aa92be2
dkostadinov <d78mdd@gmail.com>
2025-04-26 23:52:59 +0300

    - initial frame - initial Diagonals implementation

--- Battleship field validator/Program.cs
+++ Battleship field validator/Program.cs
@@ -12,11 +12,97 @@ namespace Battleship_field_validator
 
     public class BattleshipField
     {
+        public static Dictionary<int, int> shipsCounts = new Dictionary<int, int>();
+
+        public static int[,] board;
+
         public static bool ValidateBattlefield(int[,] field)
         {
-            // Write your magic here
-            return true;
+            shipsCounts = new Dictionary<int, int>();
+            board = field;
+
+            CountVertial();
+            CountHorizontal();
+            CountShips1();
+
+            return
+                !HasDiagonals() &&
+                Ships4 == 1 &&
+                Ships3 == 2 &&
+                Ships2 == 3 &&
+                Ships1 == 4 &&
+                ShipsLong == 0;
         }
 
+        public static int ShipsLong =>
+            shipsCounts
+                .Where(pair => pair.Key > 4)
+                .Sum(pair => pair.Value);
+
+        public static int Ships1 => shipsCounts[1];
+
+        public static int Ships2 => shipsCounts[2];
+
+        public static int Ships3 => shipsCounts[3];
+
+        public static int Ships4 => shipsCounts[4];
+
+        private static bool HasDiagonals()
+        {
+            bool result = false;
+
+            for (var row = 0; row < board.GetLength(0); row++)
+                for (var col = 0; col < board.GetLength(1); col++)
+                {
+                    if (board[row, col] == 1) // ship piece
+                    {
+                        if (row == board.GetLength(0) - 1) // last row
+                        {
+                            break; // no diagonals to check
+                        }
+
+                        if (col == 0) // 1st - starting scell
+                        {
+                            if (board[row + 1, col + 1] == 1) // ship piece
+                            {
+                                return true;
+                            }
+                        }
+                        else if (col == board.GetLength(1) - 1) // 10th - last cell
+                        {
+                            if (board[row + 1, col - 1] == 1) // ship piece
+                            {
+                                return true;
+                            }
+                        }
+                        else // 2nd to 9th cells
+                        {
+                            if (board[row + 1, col + 1] == 1 ||
+                                board[row + 1, col - 1] == 1) // ship piece
+                            {
+                                return true;
+                            }
+                        }
+                    }
+
+                }
+
+            return result;
+        }
+
+        private static void CountShips1()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void CountHorizontal()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void CountVertial()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
